539279ca1118bcc52a05b2d479f7c4bcf92548ed
ishrak-liberatelabs <ishrak@liberate-labs.com>
2023-10-22 23:45:03 +0600

    delete option added

--- index.js
+++ index.js
@@ -83,6 +83,14 @@ async function run() {
 
         });
 
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await booking.deleteOne(query);
+            res.send(result)
+
+        })
+
 
 
         // Send a ping to confirm a successful connection
