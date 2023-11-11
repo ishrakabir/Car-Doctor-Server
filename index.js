40c50bd89bb97ca77f86910c9ca6cae5f389f0fb
ishrak-liberatelabs <aatharishrak@gmail.com>
2023-11-11 15:43:56 +0600

    credential added

--- index.js
+++ index.js
@@ -25,6 +25,23 @@ const client = new MongoClient(uri, {
     }
 });
 
+const verifyJWT = (req, res, next) => {
+    console.log("Hitting JWT");
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: "unauthorized access" })
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.JWT_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ error: true, message: "unauthorized access" })
+        }
+        req.decoded = decoded;
+        next();
+    });
+
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -35,10 +52,10 @@ async function run() {
 
         app.post('/jwt', (req, res) => {
             const user = req.body;
-            console.log('user',user);
+            console.log('user', user);
             const token = jwt.sign(user, process.env.JWT_TOKEN, { expiresIn: '1h' })
             console.log('token', token);
-            res.send({token});
+            res.send({ token });
         })
 
         app.get('/services', async (req, res) => {
@@ -76,17 +93,22 @@ async function run() {
         });
 
         app.post('/bookings', async (req, res) => {
+
             const service = req.body;
             const result = await booking.insertOne(service);
             res.send(result);
         })
 
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            console.log("decoded", req.decoded);
+            if (decoded.email !== req.query.email) {
+                return res.status(401).send({ error: true, message: "forbidden access" })
+            }
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
             }
-            console.log(query);
             const result = await booking.find(query).toArray();
             res.send(result);
 
